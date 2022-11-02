14f98f2e47d115e33fbd9327a2730c1262926ee4
Mihanakis <Mavrionakis@gmail.com>
2022-11-02 17:32:23 +0300

    Добавлена функция превращения списка в матрицу. Закончена функция проверки на ошибки. В работе функция вычислений внутри матрицы в общем виде.

--- Lab1/Lab1.4.py
+++ Lab1/Lab1.4.py
@@ -1,13 +1,9 @@
-BYTES_ONE_CHAR = 1  # размер одного символа в байтах
+a = list(range(10))
 
-# никаких магических чисел
-pages = 100
-lines = 50
-chars = 25
+b = 5
 
-total_chars = chars * lines # символов на странице книги
-total_bytes = total_chars * pages # размер книги в байтах
-
-disk_size = 1.44 * (2**20) # размер дискеты в байтах
-
-print(disk_size // total_bytes)  # количество книг на дискете
+for d in a:
+    if d > b:
+        if d < 8:
+            continue
+    print(d)
