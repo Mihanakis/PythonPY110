1699682996eca13fbf01d6df0541a8a78f693c40
Mihanakis <Mavrionakis@gmail.com>
2022-11-12 10:39:31 +0300

    Переписано в виде List comprehension

--- Lab4/Lab 4.1.py
+++ Lab4/Lab 4.1.py
@@ -1,7 +1,5 @@
 from pprint import pprint
 
 
-dict_list = []
-for n in range(16):
-    dict_list += [{'bin': bin(n)} | {'dec': n} | {'oct': oct(n)} | {'hex': hex(n)}]
+dict_list = [{'bin': bin(n)} | {'dec': n} | {'oct': oct(n)} | {'hex': hex(n)} for n in range(16)]
 pprint(dict_list)
