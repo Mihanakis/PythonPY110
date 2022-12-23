4a6aa4254f1ed71a0394944113a648407dbfdc49
Mihanakis <115340358+Mihanakis@users.noreply.github.com>
2022-12-23 14:23:47 +0300

    Правки по LAB4
    Учёл наличие нескольких пробелов между словами и добавил #center после ссылки на картинку чтобы ответы больше сходились. Всё еще присутствует разница в кодировке, преимущественно в описании книги и именах.

---
+++ Lab4.py
@@ -0,0 +1,17 @@
+import json
+import re
+
+BOOKS_FILE = "books.md"
+BOOK_REGEX = r"""####\s+(?P<position>\d+).\s+\[(?P<book>.+?)\]+\((?P<book_url>.+?)\)\s+by\s+(?P<author>.+?)\s+\((?P<recommended>\d+\.?\d+\%)\s+recommended\)\n?\n?.+?\((?P<cover_url>http[s]?.+?png#center).+?\"(?P<description>.+?)\""""
+
+
+def task():
+    book_pattern = re.compile(BOOK_REGEX, re.DOTALL)  # флаг re.DOTALL описывает, что под символом точка может содержаться символ переноса строки
+
+    with open(BOOKS_FILE) as f:
+        for book in book_pattern.finditer(f.read()):
+            print(json.dumps(book.groupdict(), indent=4))
+
+
+if __name__ == '__main__':
+    task()
